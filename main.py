688b9ef5e8b560a1506816687bd6237547e975fd
zanmiks-ctrl <zanmiks@gmail.com>
2025-12-17 09:09:03 +0200

    viss kods done
    done

--- main.py
+++ main.py
@@ -9,40 +9,43 @@ obj = viz.add('kicen.glb')
 obj.setScale([0.1, 0.1, 0.1])
 obj.setPosition([0, 0, 5])
 
-vizcam.WalkNavigate()
 
-viz.MainView.setPosition([0, 6, 0])
+vizcam.WalkNavigate()
+viz.MainView.collision(True)
+viz.MainView.gravity(0)
+viz.MainView.setPosition([0,4.5,0])
+
 
 viz.mouse.setVisible(True)
 viz.mouse.setTrap(True)
 
 kruze = viz.add('kruze.glb', parent=obj)
-kruze.setPosition([34.5, 33, -1.8])
+kruze.setPosition([34.5, 32, -1.8])
 kruze.setScale([0.1, 0.1, 0.1])
 kruze.setEuler([90, 0, 0])
 
 cukurs = viz.add('cukurs.glb', parent=obj)
-cukurs.setPosition([46, 42, 93])
+cukurs.setPosition([46, 41.5, 93])
 cukurs.setScale([0.2, 0.2, 0.2])
 
 
 kafija = viz.add('kafija.glb', parent=obj)
-kafija.setPosition([23.5, 42, 93.5])
+kafija.setPosition([23.5, 41.5, 93.5])
 kafija.setScale([0.4, 0.4, 0.4])
 
 
 piens = viz.add('piens.glb', parent=obj)
-piens.setPosition([-80, 42, 25])
+piens.setPosition([-80, 41, 25])
 piens.setScale([0.2, 0.2, 0.2])
 piens.setEuler([110, 0, 0])
 
 tejkanna= viz.add('tejkanna.glb', parent=obj)
-tejkanna.setPosition([0, 42, 85])
+tejkanna.setPosition([0, 41, 85])
 tejkanna.setScale([0.2, 0.2, 0.2])
 tejkanna.setEuler([-60, 0, 0])
 
 smalcinatajs= viz.add('smalcinatajs.glb', parent=obj)
-smalcinatajs.setPosition([-80, 42, 70])
+smalcinatajs.setPosition([-80, 41, 70])
 smalcinatajs.setScale([0.3, 0.3, 0.3])
 smalcinatajs.setEuler([-60, 0, 0])
 
